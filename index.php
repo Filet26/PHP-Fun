e99cc673ec7bf669e1e33ae83d2ec3eb67e54806
Desmond Ho <kakacake666@gmail.com>
2022-12-13 14:24:58 -0800

    outputs

---
+++ index.php
@@ -0,0 +1,20 @@
+<!-- 01_outputs -->
+
+<? 
+// echos stuff, strings,numbers, html
+echo "Hello LOL! This is PHP! ", 900, " POG!";
+
+// print, only 1 argument 
+print " Hello This is print";
+
+// print_R() -> single values and arrays
+print_r([1, 2, 3]);
+
+// var dump, shows data type and length
+var_dump("1234");
+
+// var_export, srtring rep of variable
+var_export("test");
+?>
+
+<!-- END ouyputs -->
