724825f8b7254cba338302586e477374725504ce
Desmond Ho <kakacake666@gmail.com>
2022-12-14 21:20:51 -0800

    bunch of stuff

--- index.php
+++ index.php
@@ -240,19 +240,256 @@ foreach ($colors as $index => $color) {
 
 <!-- 06_functions -->
 
+<?php 
+// PHP functions\
 
+// default value of steven
+function myfunc($name="Steven") {
+
+    echo "Hello ".$name."<br>";
+}
+;
+
+myfunc("Bobby");
+
+$add = fn($n1, $n2) => $n1 + $n2;
+
+echo  $add(10, 3)
+
+?>
 
 <!-- END functions -->
 
 <!-- =========================================================== -->
 
+<!-- 07_array_functions -->
+
+<?php
+
+$games = ["Fortnite", "Valorant", "COD", "Roblox"];
+
+// length of array
+
+echo count($games);
+
+
+// search array for a value, returns T/F (1 or 0)
+
+echo in_array("COD", $games);
+
+// add item to an array
+
+// add to end
+$games[] = "Minecraft";
+array_push($games, "GTA V");
+
+// add to beginning
+array_unshift($games, "Kovaaks");
+
+print_r($games);
+
+
+// Remove from an array
+
+// POP last item
+array_pop($games);
+
+// pop last item
+array_shift($games);
+
+// remove based on index, removes index too lmao
+
+unset($games[1]);
+
+// Split
+
+$chunked_Arr = array_chunk($games, 2);
+
+print_r($chunked_Arr);
+
+// concatnate
+
+$list1 = [1, 2, 3];
+$list2 = [4, 5, 6];
+
+$list_merge = array_merge($list1, $list2);
+
+print_r($list_merge);
+
+// range
+
+$nums = range(1, 20);
+
+print_r($nums);
+
+
+// map
+$newNums = array_map(function ($num) {
+    return "Number {$num}";
+}, $nums);
+
+print_r($newNums);
+
+// filter
+
+$lessthan10 = array_filter($nums, fn($num) => $num <= 10);
+
+print_r($lessthan10);
+
+// reduce, whatever very nasty
+
+?>
+
+<!-- END array functions -->
 
 
 
 <!-- =========================================================== -->
+
+
+<!-- 08_string_functions -->
+<?php
+
+$string = 'Hello World';
+
+// Get the length of a string
+echo strlen($string);
+
+// Find the position of the first occurrence of a substring in a string
+echo strpos($string, 'o');
+
+// Find the position of the last occurrence of a substring in a string
+echo strrpos($string, 'o');
+
+// Reverse a string
+echo strrev($string);
+
+// Convert all characters to lowercase
+echo strtolower($string);
+
+// Convert all characters to uppercase
+echo strtoupper($string);
+
+// Uppercase the first character of each word
+echo ucwords($string);
+
+// String replace
+echo str_replace('World', 'Everyone', $string);
+
+// Return portion of a string specified by the offset and length
+echo substr($string, 0, 5);
+echo substr($string, 5);
+
+// Starts with
+if (str_starts_with($string, 'Hello')) {
+  echo 'YES';
+}
+
+// Ends with
+if (str_ends_with($string, 'ld')) {
+  echo 'YES';
+}
+
+// HTML Entities
+$string2 = '<h1>Hello World</h1>';
+echo htmlentities($string2);
+
+// Formatted Strings - useful when you have outside data
+// Different specifiers for different data types
+printf('%s is a %s', 'Brad', 'fart smella');
+printf('1 + 1 = %f', 1 + 1); // float
+
+?>
+<!-- END String_functions -->
+
+
 <!-- =========================================================== -->
+
+
+<!-- 09_superglobals -->
+<?php
+// Built in variables, available in all scopes
+/* ---------- Superglobals ---------- */
+//Built in variables that are always available in all scopes
+
+/*
+  $GLOBALS - A superglobal variable that holds information about any variables in global scope.
+  $_GET - Contains information about variables passed through a URL or a form.
+  $_POST -  Contains information about variables passed through a form.
+  $_COOKIE - Contains information about variables passed through a cookie.
+  $_SESSION - Contains information about variables passed through a session.
+  $_SERVER - Contains information about the server environment.
+  $_ENV - Contains information about the environment variables.
+  $_FILES -  Contains information about files uploaded to the script.
+  $_REQUEST - Contains information about variables passed through the form or URL.
+*/
+
+// var_dump($GLOBALS);
+// var_dump($_GET);
+// var_dump($_REQUEST);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <ul>
+    <li>Host: <?php echo $_SERVER['HTTP_HOST']; ?></li>
+    <li>Document Root: <?php echo $_SERVER['DOCUMENT_ROOT']; ?></li>
+    <li>System Root: <?php echo $_SERVER['SystemRoot']; ?></li>
+    <li>Server Name: <?php echo $_SERVER['SERVER_NAME']; ?></li>
+    <li>Server Port: <?php echo $_SERVER['SERVER_PORT']; ?></li>
+    <li>Current File Dir: <?php echo $_SERVER['PHP_SELF']; ?></li>
+    <li>Request URI: <?php echo $_SERVER['REQUEST_URI']; ?></li>
+    <li>Server Software: <?php echo $_SERVER['SERVER_SOFTWARE']; ?></li>
+    <li>Client Info: <?php echo $_SERVER['HTTP_USER_AGENT']; ?></li>
+    <li>Remote Address: <?php echo $_SERVER['REMOTE_ADDR']; ?></li>
+    <li>Remote Port: <?php echo $_SERVER['REMOTE_PORT']; ?></li>
+  </ul>
+</body>
+</html>
+<!-- END superglobals -->
+
 <!-- =========================================================== -->
+
+<!-- 10_get_post -->
+
+<?php
+
+// pass data through url and forms using the $_GET and $_POST superglobals
+
+echo $_GET['name'];
+echo $_GET['age'];
+?>
+
+<a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=Bobby&age=45">Click Me</a>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<div>
+    <label for="name">name: </label>
+    <input type="text" name="name">
+</div>
+<div>
+    <label for="age">age: </label>
+    <input type="text" name="age">
+</div>
+<input type="submit" value="Submit" name="submit">
+
+</form>
+
+<!-- END get_post -->
+
 <!-- =========================================================== -->
+
+<!-- 11_input_sanitation -->
+
+<!-- END input_sanitation -->
+
 <!-- =========================================================== -->
 <!-- =========================================================== -->
 <!-- =========================================================== -->
